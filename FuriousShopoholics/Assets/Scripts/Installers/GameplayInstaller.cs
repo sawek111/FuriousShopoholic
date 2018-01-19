9c71ce90593f7c9cc3c79fb65ac57372eac522b4
Saper <sawek111@gmail.com>
2018-01-19 15:06:25 +0100

    <gameplay scene> basic stuff

---
+++ FuriousShopoholics/Assets/Scripts/Installers/GameplayInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Zenject;
+
+public class GameplayInstaller : MonoInstaller<GameplayInstaller>
+{
+    [Inject] private Settings _settings = null;
+
+    public override void InstallBindings()
+    {
+       
+    }
+
+    [Serializable]
+    public class Settings
+    {
+
+    }
+}
--- FuriousShopoholics/Assets/Scripts/Scriptable/GameSettings.cs
+++ FuriousShopoholics/Assets/Scripts/Scriptable/GameSettings.cs
@@ -5,10 +5,13 @@ using Zenject;
 public class GameSettings : ScriptableObjectInstaller<GameSettings>
 {
     public MenuInstaller.Settings menuSettings;
+    public GameplayInstaller.Settings gameplaySettings;
 
     public override void InstallBindings()
     {
         Container.BindInstance(menuSettings);
+        Container.BindInstance(gameplaySettings);
+
         return;
     }
 }
