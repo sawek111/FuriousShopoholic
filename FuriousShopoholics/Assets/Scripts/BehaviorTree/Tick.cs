f1f200c1bb9b7b70eaa8f5acf9a25fb59d0e3768
Saper <sawek111@gmail.com>
2018-01-21 13:05:56 +0100

    <BehaviourTree> Skeleton

---
+++ FuriousShopoholics/Assets/Scripts/BehaviorTree/Tick.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tick 
+{
+    private BehaviorTree _tree;
+    private BlackBoard _board;
+    private object _target;
+    private int _nodesCount;
+    private List<Node> _openNodes;
+
+    public List<Node> OpenNodes
+    {
+        get { return _openNodes; }
+    }
+
+    public int NodesCount
+    {
+        get { return _nodesCount; }
+    }
+
+    public object Target
+    {
+        get { return _target; }
+        set { _target = value; }
+    }
+
+    public BlackBoard Board
+    {
+        get { return _board; }
+    }
+
+    public Tick(BehaviorTree tree, BlackBoard board, object target)
+    {
+        _target = target;
+        _board = board;
+        _tree = tree;
+        _nodesCount = 0;
+        _openNodes = new List<Node>();
+
+        return;
+    }
+
+    public void EnterNode(Node node)
+    {
+        _nodesCount++;
+        _openNodes.Add(node);
+
+        return;
+    }
+
+    public void OpenNode(Node node)
+    {
+        //On create
+    }
+
+
+    public NodeState TickNode(Node node)
+    {
+        //TODO
+        return NodeState.ERROR;
+    }
+
+    public void CloseNode(Node node)
+    {
+        if (_openNodes.Contains(node))
+        {
+            _nodesCount--;
+            _openNodes.Remove(node);
+        }
+
+        return;
+    }
+
+    public void ExitNode(Node node)
+    {
+        //On Exit
+    }
+
+}
---
+++ FuriousShopoholics/Assets/Scripts/Utils/Enums.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region BehaviorTree
+public enum NodeState
+{
+    SUCCESS,
+    FAILURE,
+    RUNNING,
+    ERROR
+}
+
+public enum NodeType
+{
+    COMPOSITE,
+    DECORATOR,
+    LEAF,
+    ROOT
+}
+
+public enum LeafType
+{
+    CONDITION,
+    ACTION
+}
+
+public enum MemoryKeys
+{
+    OPEN_NODES,
+    NODES_COUNT
+}
+#endregion
+
+public enum ShopaholicAnimations
+{
+    Idle,
+    Running,
+    Attack,
+    Agony,
+    Escape,
+    Die
+}
+
