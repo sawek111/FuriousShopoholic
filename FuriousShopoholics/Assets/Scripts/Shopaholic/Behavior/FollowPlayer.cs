06fd82b661ad7a111f7262e7bc7b644d052d0f3f
Saper <sawek111@gmail.com>
2018-01-22 14:41:27 +0100

    <ignore>

--- FuriousShopoholics/Assets/Scripts/Shopaholic/Behavior/FollowPlayer.cs
+++ FuriousShopoholics/Assets/Scripts/Shopaholic/Behavior/FollowPlayer.cs
@@ -7,7 +7,6 @@ public class FollowPlayer : ShopaholicBTLeaf
     public override NodeState ParticularTick(Tick tick)
     {
         _shopaholic.FollowPlayer();
-        Debug.LogWarning("Follow");
         return NodeState.SUCCESS;
     }
 }
